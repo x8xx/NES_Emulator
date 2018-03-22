d9e0d189cf0acb9d074069a443c1ef4c2d2cce3b
偏差値24 <t.29.wuq@gmail.com>
2018-03-23 04:39:56 +0900

    first

---
+++ NES_Emulator/NES_Emulator/NES/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace NES_Emulator.NES
+{
+    public class Controller
+    {
+        
+
+        public Controller()
+        {
+        }
+
+        public byte ReadIoRegister(ushort address)
+        {
+            switch(address)
+            {
+                case 0x4016:
+                    break;
+                case 0x4017:
+                    break;
+            }
+            return 0;
+        }
+
+    }
+}
