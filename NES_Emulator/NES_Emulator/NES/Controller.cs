f564e75908220606a6cf0ce4028e9d5be567679a
偏差値24 <t.29.wuq@gmail.com>
2018-04-16 15:49:46 +0900

    fix : ok

--- NES_Emulator/NES_Emulator/NES/Controller.cs
+++ NES_Emulator/NES_Emulator/NES/Controller.cs
@@ -10,7 +10,7 @@ namespace NES_Emulator.NES
 		Dictionary<ushort, int> controller; //IOレジスタ
 		Dictionary<ushort, int> controllerWriteCount; //IOレジスタ書き込み回数
 		Dictionary<ushort, int> controllerReadCount; //IOレジスタ読み込み回数
-		Dictionary<ushort, Input[]> isControllerInput; //コントローラー入力状態
+		Dictionary<ushort, bool[]> isControllerInput; //コントローラー入力状態
 		bool waitKey; //key入力待ち
         
 		public Controller()
@@ -18,12 +18,7 @@ namespace NES_Emulator.NES
 			controller = new Dictionary<ushort, int> { { 0x4016, 0 }, { 0x4017, 0 } };
 			controllerWriteCount = new Dictionary<ushort, int> { { 0x4016, 0 }, { 0x4017, 0 } };
 			controllerReadCount = new Dictionary<ushort, int> { { 0x4016, 0 }, { 0x4017, 0 } };
-			isControllerInput = new Dictionary<ushort, Input[]> { { 0x4016, new Input[8] }, { 0x4017, new Input[8] } };
-			for (int i = 0; i < 8; i++)
-			{
-				isControllerInput[0x4016][i] = new Input() { isInput = false, isRead = true };
-				isControllerInput[0x4017][i] = new Input() { isInput = false, isRead = true };
-			}
+			isControllerInput = new Dictionary<ushort, bool[]> { { 0x4016, new bool[8] }, { 0x4017, new bool[8] } };
 			waitKey = false;
 		}
         
@@ -34,8 +29,8 @@ namespace NES_Emulator.NES
         /// <param name="address">Address.</param>
 		public bool ReadIoRegister(ushort address)
 		{
-			bool input = isControllerInput[address][controllerReadCount[address]].isInput;
-			isControllerInput[address][controllerReadCount[address]].isRead = true;
+			bool input = isControllerInput[address][controllerReadCount[address]];
+			isControllerInput[address][controllerReadCount[address]] = false;
 			if (input)
 				waitKey = false;
 			controllerReadCount[address]++;
@@ -60,65 +55,38 @@ namespace NES_Emulator.NES
 				case 1:
 					if (controller[address] == 1 && value == 0)
 					{
+						controllerReadCount[address] = 0;
 						for (int i = 0; i < isControllerInput[address].Length; i++)
 						{
-							if (!isControllerInput[address][i].isRead)
-								break;
-							Debug.WriteLine(isControllerInput[address][i].isInput);
-							isControllerInput[address][i].isInput = false;
-							isControllerInput[address][i].isRead = false;
+							if (isControllerInput[address][i])
+								continue;
+							isControllerInput[address][i] = false;
 						}
 					}
 					controllerWriteCount[address] = 0;
 					break;
 			}
 		}
-
+        
         /// <summary>
         /// Key入力
+		/// 0, A
+		/// 1, B
+		/// 2, SELECT
+		/// 3, START
+		/// 4, UP
+		/// 5, DOWN
+		/// 6, LEFT
+		/// 7, RIGHT
         /// </summary>
         /// <param name="player">1P or 2P</param>
         /// <param name="key">Key</param>
-		public void InputKey(int player, string key)
+		public void InputKey(int player, int key)
 		{
 			if (!waitKey)
 			{
 				ushort address = (ushort)(0x4015 + player);
-                switch (key)
-                {
-                    case "A":
-                        isControllerInput[address][0].isInput = true;
-						isControllerInput[address][0].isRead = false;
-                        break;
-                    case "B":
-                        isControllerInput[address][1].isInput = true;
-						isControllerInput[address][1].isRead = false;
-                        break;
-                    case "SELECT":
-                        isControllerInput[address][2].isInput = true;
-						isControllerInput[address][2].isRead = false;
-                        break;
-                    case "START":
-                        isControllerInput[address][3].isInput = true;
-						isControllerInput[address][3].isRead = false;
-                        break;
-                    case "UP":
-                        isControllerInput[address][4].isInput = true;
-						isControllerInput[address][4].isRead = false;
-                        break;
-                    case "DOWN":
-                        isControllerInput[address][5].isInput = true;
-						isControllerInput[address][5].isRead = false;
-                        break;
-                    case "LEFT":
-                        isControllerInput[address][6].isInput = true;
-						isControllerInput[address][6].isRead = false;
-                        break;
-                    case "RIGHT":
-                        isControllerInput[address][7].isInput = true;
-						isControllerInput[address][7].isRead = false;
-                        break;
-                }
+				isControllerInput[address][key] = true;
                 waitKey = true;
 			}
 		}
